0ed66464c3a38b83459dbbb532d1f2a02a0d6b52
GLITCH <theslickprogrammer@gmail.com>
2025-09-08 15:38:20 +0100

    - Added a tie checker in playloop game_instance(), made sure winner check comes before tie check - Added emojis

--- tic-tac-toe game.py
+++ tic-tac-toe game.py
@@ -22,8 +22,9 @@ class Game:
                 Game.board[move -1] = self.move_type # set the move on the board
                 Game.play_count += 1 # increment number of plays done by one 
                 Game.played_grids.append(move) # add played index to played_grids
+        
         else:
-            print("\nEnter a valid grid position")
+            print("\n⚠️ Enter a valid grid position")
 
     winner_found = False
 
@@ -32,32 +33,32 @@ class Game:
         empty_grid = " "
         # horizontal grid check
         if Game.board[0] == Game.board[1] == Game.board[2] != empty_grid:
-            print(f"{Game.board[0]} wins (horizontal checkmate)")
+            print(f"{Game.board[0]} wins (horizontal checkmate)🎉 ")
             Game.winner_found = True
         elif Game.board[3] == Game.board[4] == Game.board[5] != empty_grid:
-            print(f"{Game.board[3]} wins (horizontal checkmate)")
+            print(f"{Game.board[3]} wins (horizontal checkmate)🎉 ")
             Game.winner_found = True
         elif Game.board[6] == Game.board[7] == Game.board[8] != empty_grid:
-            print(f"{Game.board[6]} wins (horizontal checkmate)")
+            print(f"{Game.board[6]} wins (horizontal checkmate)🎉 ")
             Game.winner_found = True
 
         # vertical grid check
         elif Game.board[0] == Game.board[3] == Game.board[6] != empty_grid:
-            print(f"{Game.board[0]} wins (vertical checkmate)")
+            print(f"{Game.board[0]} wins (vertical checkmate)🎉 ")
             Game.winner_found = True
         elif Game.board[1] == Game.board[4] == Game.board[7] != empty_grid:
-            print(f"{Game.board[1]} wins (vertical checkmate)")
+            print(f"{Game.board[1]} wins (vertical checkmate)🎉 ")
             Game.winner_found = True
         elif Game.board[2] == Game.board[5] == Game.board[8] != empty_grid:
-            print(f"{Game.board[2]} wins (vertical checkmate)")
+            print(f"{Game.board[2]} wins (vertical checkmate)🎉 ")
             Game.winner_found = True
 
         # diagonal grid check
         elif Game.board[0] == Game.board[4] == Game.board[8] != empty_grid:
-            print(f"{Game.board[0]} wins (diagonal checkmate)")
+            print(f"{Game.board[0]} wins (diagonal checkmate)🎉 ")
             Game.winner_found = True
         elif Game.board[6] == Game.board[4] == Game.board[2] != empty_grid:
-            print(f"{Game.board[0]} wins (diagonal checkmate)")
+            print(f"{Game.board[0]} wins (diagonal checkmate)🎉 ")
             Game.winner_found = True
         else:
             print(f"\nTotal moves played: {Game.play_count}")
@@ -80,10 +81,17 @@ def game_instance():
 
         if Game.winner_found == True:
             break
+        if Game.play_count >= 9:
+            print("Tie")
+            break
+
         player2.make_move()
         Game.winner_checker()
 
         if Game.winner_found == True:
+            break
+        if Game.play_count >= 9:
+            print("Tie")
             break
     
 
