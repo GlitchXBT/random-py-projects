f8b18e2df37aeab723efd19d9eaf3b0c278657bf
GLITCH <theslickprogrammer@gmail.com>
2025-09-08 13:17:20 +0100

    Refactored game structure: encapsulated logic in Game class, added move validation, dynamic board updates, and initial winner check

--- tic-tac-toe game.py
+++ tic-tac-toe game.py
@@ -1,57 +1,66 @@
-class Players:
+class Game:
+    board = [" "," "," "," "," "," "," "," "," "]
+    play_count = 0
+    playables = (1,2,3,4,5,6,7,8,9)
+    played_grids = []
+    winner_found = False
+
+    @staticmethod
+    def grid():
+        grid  = (f"\n{Game.board[0]}|{Game.board[1]}|{Game.board[2]}\n-----\n{Game.board[3]}|{Game.board[4]}|{Game.board[5]}\n-----\n{Game.board[6]}|{Game.board[7]}|{Game.board[8]}")
+        print(grid)
+
+
     def __init__(self,move_type, name):
         self.move_type = move_type
         self.name =  name
         
-
-    def get_move(self):
-        move = int(input(f"{self.name} >>> "))
-        if 0 < move <=9:
-            return move
+    
+    def make_move(self):
+        move = int(input(f"\n{self.name},you are {self.move_type} >>> "))
+    
+        if move in Game.playables and move not in Game.played_grids:
+                Game.board[move -1] = self.move_type # set the move on the board
+                Game.play_count += 1 # increment number of plays done by one 
+                Game.played_grids.append(move) # add played index to played_grids
         else:
-            print("invalid move")
-    
+            print("\nEnter a valid grid position")
 
-board = [" "," "," "," "," "," "," "," "," "]
-
-player1 = Players(move_type="x", name="playerOne")
-player2 = Players(move_type="o", name="playerTwo")
-
-def print_grid():
-    grid  = (f"{board[0]}|{board[1]}|{board[2]}\n-----\n{board[3]}|{board[4]}|{board[5]}\n-----\n{board[6]}|{board[7]}|{board[8]}")
-    print(grid)
-
-    
-
-
-
-Game_over = False
-playables = (1,2,3,4,5,6,7,8,9)
-play_count = 0
-
-print_grid()
-
-while Game_over == False:
-
-    player1_move = player1.get_move()
-    if player1_move in playables:
-        board[player1_move -1] = "X"
-
-        play_count += 1
-
-        print_grid()
-
+    @staticmethod
+    def winner_checker():
+        if Game.board.count[0] == Game.board[1] == Game.board[3]:
+            print(f"{Game.board[0]} wins")
+            Game.winner_found = True
+        else:
+            print(f"The current game's play count is {Game.play_count}")
         
-    else:
-        print("Enter a valid grid position")
-    
-    player2_move = player2.get_move()
-    if player2_move in playables:
-        board[player2_move -1] = "O"
-        play_count += 2
 
-        print_grid()    
-    Game_over = True
+player1 = Game(move_type="x", name="playerOne")
+player2 = Game(move_type="o", name="playerTwo")
+
+
+# playloop
+def game_instance():
+    
+    Game.grid()
+
+    for i in str(Game.playables) :
+        player1.make_move()
+        Game.grid()
+        if Game.play_count >= 3:
+            Game.winner_checker()
+        player2.make_move()
+        Game.grid()
+        if Game.play_count >= 3:
+            Game.winner_checker()
+    
+    
+
+
+
+
+game_instance()
+
 
 
 
